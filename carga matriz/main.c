2e7d63ec2a6dc6208d8ead8d0d5bc3a5cd5a8b59
agustin modia <agustinmodiamoleracing@gmail.com>
2017-09-08 21:39:51 -0300

    h
    h

--- carga matriz/main.c
+++ carga matriz/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 int valcadena(char[], int );
+int mostrarnombres(int h, char[][h], int );
+int ordenonombres(int hh, char[][hh], int );
+int mostrarnombre(char []);
 int main()
 {
     char aux [50];
     char matriz[5][20];
     int i = 0;
+    int h = 0;
 
 
     for(i=0; i<5; i++)
@@ -23,10 +27,11 @@ int main()
         strcpy(matriz[i], aux);
 
     }
-  for(i=0; i<5; i++)
-    {
-        printf("su nombre es: %s \n", matriz[i]);
-    }
+
+mostrarnombres(20, matriz, 5);
+printf("\n\n");
+ordenonombres(20, matriz, 5);
+mostrarnombres(20, matriz, 5);
 
 
             return 0;
@@ -44,4 +49,46 @@ int valcadena(char nombre [], int tam)
 
     return h;
 
+}
+
+int mostrarnombres(int h, char nombre[][h], int tam)
+{
+    int i = 0;
+  for(i=0; i<tam; i++)
+    {
+       mostrarnombre(nombre[i]);
+    }
+    return 1;
+}
+
+
+
+int mostrarnombre(char nombre[])
+{
+
+        printf("su nombre es: %s \n", nombre);
+
+    return 1;
+}
+
+int ordenonombres(int hh, char matriz[][hh], int tam)
+{
+    int i = 0;
+    int h = 0;
+    char aux[20];
+   for(i=0; i<4; i++)
+    {
+         for(h=i+1; h<5; h++)
+    {
+        if( strcmp(matriz[i], matriz[h])>0  )
+            {strcpy(aux,matriz[i]);
+            strcpy(matriz[i],matriz[h]);
+                  strcpy(matriz[h],aux);
+}
+    }
+
+
+
+    }
+    return 1;
 }
